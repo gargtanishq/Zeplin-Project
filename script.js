867a41d7ddf2e97c13d503e7f6db53a144d8317c
gargtansihq <tanishq.garg20gmail.com>
2021-09-23 23:04:41 +0530

    Javascript implemented on searchbar, sort and next-prev btn under recommended grps

---
+++ script.js
@@ -0,0 +1,87 @@
+// js file
+
+// select groups move left and right under recommendation div
+
+const scrollButtonRight = document.getElementById("scrollPageRight");
+const scrollButtonLeft = document.getElementById("scrollPageLeft");
+
+scrollButtonRight.onclick = function () {
+  document.getElementById("selectGroupsId").scrollLeft += 100;
+};
+scrollButtonLeft.onclick = function () {
+  document.getElementById("selectGroupsId").scrollLeft -= 100;
+};
+
+// sorting the table content
+
+function sortTableByGroupName(table, column, asc = true) {
+  // checking if asc or not
+  const dirModifier = asc ? 1 : -1;
+  // single tbody in case we have many tbody in a table
+  const tBody = table.tBodies[0];
+  // converts the data into arry form list
+  const rows = Array.from(tBody.querySelectorAll("tr"));
+
+  // sort each row
+  const sortedRows = rows.sort((a, b) => {
+    const aColText = a.querySelector("td").textContent.trim();
+    const bColText = b.querySelector("td").textContent.trim();
+
+    // if td1 > td2 then multiply by 1 to dirModifier to remain as it is and if not then multiply by -1 for desending
+    return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+  });
+
+  //   remove all the existing trs from the table
+  while (tBody.firstChild) {
+    tBody.removeChild(tBody.firstChild);
+  }
+
+  //   Now adding trs back in the table after sorting
+  tBody.append(...sortedRows);
+
+  //   remember the sorting order
+  table
+    .querySelectorAll("th")
+    .forEach((th) => th.classList.remove("th-sort-asc", "th-sort-desc"));
+  //   toggle between the sort classes
+  table.querySelector("th").classList.toggle("th-sort-asc", asc);
+  table.querySelector("th").classList.toggle("th-sort-desc", !asc);
+}
+
+let whichSort = document.getElementById("sorting");
+// calling the function on click to trigger the sorting
+whichSort.addEventListener("click", function () {
+  // gets the element to check which class is implemented on it
+  let checkOrder = document.getElementsByTagName("th")[0];
+  let currentIsAscending = checkOrder.classList.contains("th-sort-asc");
+  let tableElement = document.querySelector("table");
+  sortTableByGroupName(tableElement, 1, !currentIsAscending);
+});
+
+// search bar
+
+function searchGroupName() {
+  let input = document.getElementById("inputGroupName").value;
+  input = input.toLowerCase();
+
+  // getting the th from tbody
+  let tableElement = document.querySelector("table");
+  let tableBody = tableElement.tBodies[0];
+  let rows = Array.from(tableBody.querySelectorAll("tr"));
+
+  //  looping in the rows tr data
+  rows.forEach((element) => {
+    // getting the text only from tr
+    const extractGroupNameText = element
+      .querySelector("td")
+      .textContent.trim()
+      .toLowerCase();
+    console.log(extractGroupNameText);
+    // checks the input with the table data
+    if (!extractGroupNameText.includes(input)) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "";
+    }
+  });
+}
